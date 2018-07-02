7d10cfc4996f2369ba100bdbc4347f15a4baf737
mahitha-raj <mahiraj@tamu.edu>
2018-07-02 11:37:38 -0500

    First commit

---
+++ demo_pypackage/__init__.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3.6
+
+def story():
+    return (u'The cow lived on the farm'
+            u'The cow said hi moo')
+            
+            
---
+++ setup.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3.6
+
+from setuptools import setup
+
+setup(name='demo_pypackage',
+      version='0.0.1',
+      description='just a farm story',
+      url='http://github.com/mahitha-raj/demo_pypackage',
+      author='Mahitha Rajendran',
+      author_email='mahiraj@tamu.edu',
+      license='MIT',
+      packages=['demo_pypackage'])
+
